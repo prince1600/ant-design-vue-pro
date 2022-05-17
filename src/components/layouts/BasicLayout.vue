33f4047aafa740dec41583548cf1e5a86d0b7806
gqs <Robson_de_Souza@qq.com>
2022-05-18 01:57:00 +0800

    基本布局

--- src/components/layouts/BasicLayout.vue
+++ src/components/layouts/BasicLayout.vue
@@ -1,16 +1,64 @@
 <template>
-  <div>
-    basic layout
-    <router-view></router-view>
-  </div>
+  <a-layout id="components-layout">
+    <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+      <SideMenu/>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <a-icon
+          class="trigger"
+          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+          @click="() => (collapsed = !collapsed)"
+        />
+        <Header/>
+      </a-layout-header>
+      <a-layout-content
+        :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '280px' }"
+      >
+        Content
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        <Footer/>
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
 </template>
 
 <script>
-export default {
+import Footer from './Footer.vue'
+import Header from './Header.vue'
+import SideMenu from './SideMenu.vue'
 
+export default {
+  components: { Footer, Header, SideMenu },
+  data() {
+    return {
+      collapsed: false
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+#components-layout {
+  height: 100vh;
+}
+#components-layout .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
 
+#components-layout .trigger:hover {
+  color: #1890ff;
+}
+
+#components-layout .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
 </style>
---
+++ src/components/layouts/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Ant Design Pro ©2022 Created by Ant UED</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
